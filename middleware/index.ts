0ede04837b4dd403c9bb779bc947bc77608d9f03
Carlos Knopel <carlos.knopel@gmail.com>
2023-02-10 17:15:47 -0300

    fix

---
+++ middleware/index.ts
@@ -0,0 +1,11 @@
+import { NextResponse, URLPattern } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const pattern = new URLPattern("/:slug", req.url);
+  const slug = pattern.exec(req.url)?.pathname?.groups?.slug;
+  if (slug) {
+    // Do rewrite/redirect here
+    return NextResponse.redirect(new URL(`/${slug}`, req.url));
+  }
+}
